1fe1ba7be5679a2a7b4d1d8a09ff2a1e424aa155
benspector-mls <ben@marcylabschool.org>
2024-10-17 14:07:19 -0400

    updating examples

--- 0-basics_index.js
+++ 0-basics_index.js
@@ -1,25 +1,41 @@
-// Create object use literal notation
-const car = {
-  name: 'Camry',
-  maker: 'Toyota',
-};
+const dictionary = {
+  hello: "a casual greeting",
+  rainbow: "a colorful arc of light",
+  cat: "the superior pet",
+  "see ya": "a casual way to say 'see you later'"
+}
 
-// add key/value pairs
-car.color = 'blue'; // dot notation
-car['model-year'] = 2018; // bracket notation
+const user = {
+  "userId": 44292,
+  "username": 'c0d3rkid',
+  "password": 'jswiz1234',
+  "friends": ['iLoveSoccer123', 'pythonNinja', 'messiGOAT'],
+  "favoriteMeal": {
+    "name": 'PB&J',
+    "ingredients": ['peanut butter', 'jelly', 'bread']
+  }
+}
 
-// access values
-console.log(car.color); // blue
-console.log(car['model-year']); // 2018
+// Dot notation is the faster to type and easier to read than bracket notation
+console.log(`Hi, my name is ${user.username}`);
 
-// update values
-car.color = 'red';
-console.log(car.color); // red
+// Bracket notation requires a string to be provided
+console.log(`Hi, my name is ${user["username"]}`);
 
-// delete values
-delete car.color;
-delete car['model-year'];
+// Dot (and bracket) notation can be used to modify existing values, or create new ones!
+user.password = 'try to break in now!';
+user.isAdmin = false;
 
-// if a value doesn't exist, it's undefined
-console.log(car.color); // undefined
-console.log(car['model-year']); // undefined
+console.log(user);
+
+// If a property holds an object or array, we can use dot/bracket notation on that value!
+console.log(`My favorite meal is ${user.favoriteMeal.name}`);
+console.log(`My best friend is ${user.friends[0]}`);
+
+// Delete properties by using the `delete` keyword followed by dot/bracket notation
+delete user.userId;
+delete user["friends"];
+
+
+// Don't delete. This is used in 4-destructuring.js
+module.exports = dictionary;
